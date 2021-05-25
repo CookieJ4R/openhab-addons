b1dcbc86068c48d1888637d67d9e1de657466ae9
Kevin Jonathan Binder <kevin.jonathan.binder@mni.thm.de>
2021-05-25 18:16:59 +0200

    Initial commit

---
+++ bundles/org.openhab.binding.oktoprint/src/main/java/org/openhab/binding/oktoprint/internal/OktoPrintConfiguration.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.oktoprint.internal;
+
+/**
+ * The {@link OktoPrintConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Kevin Binder, Dario Pläschke, Florian Silber, Nour El-Dien Kamel - Initial contribution
+ */
+public class OktoPrintConfiguration {
+
+    /**
+     * Sample configuration parameters. Replace with your own.
+     */
+    public String hostname;
+    public String password;
+    public int refreshInterval;
+}
